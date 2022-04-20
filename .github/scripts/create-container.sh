00985f90028e862870d14d25440ff8ca3e484171
Thorsten Kramm <tk@system42.io>
2022-04-20 15:30:40 +0200

    30413

--- .github/scripts/create-container.sh
+++ .github/scripts/create-container.sh
@@ -14,15 +14,15 @@ POST_EXEC=("cat /etc/os-release")
 echo "🐇 Launching container now. Wait ... "
 case $CONTAINER in 
     ubuntu)
-        lxc launch ubuntu:20.04 "$CONTAINER" -q
+        lxc launch ubuntu:20.04 container -q
         POST_EXEC+=("apt-get -y update")
     ;;
     debian)
-        lxc launch images:debian/11 "$CONTAINER" -q
+        lxc launch images:debian/11 container -q
         POST_EXEC+=("apt-get -y update;apt-get -y install curl")
     ;;
     rocky)
-        lxc launch images:rockylinux/8 "$CONTAINER" -q
+        lxc launch images:rockylinux/8 container -q
         POST_EXEC+=("dnf -y install epel-release")
     ;;
     *)
@@ -33,12 +33,12 @@ esac
 echo "🚚 Container launched"
 for i in $(seq 60);do
     echo "🚴‍♂️ waiting for container to come up $i"
-    if lxc ls|grep -q "${CONTAINER}.*RUNNING.*10\."; then
+    if lxc ls|grep -q "container.*RUNNING.*10\."; then
         echo "🎉 container running"
         break
     fi
     sleep 2
 done
-echo "🐸 Executing '\$POST_EXEC' inside container $CONTAINER"
-printf "%s\n" "${POST_EXEC[@]}"|lxc exec "${CONTAINER}" -- bash
+echo "🐸 Executing '\$POST_EXEC' inside container"
+printf "%s\n" "${POST_EXEC[@]}"|lxc exec container -- bash
 
